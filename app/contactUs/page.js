6b2f355ff09379c61714d627e5ab8651c4cb0395
Webb <webbcody04@gmail.com>
2025-09-20 01:14:57 -0600

    Updated Catalogue, Contact Us, and Products pages

--- app/contactUs/page.js
+++ app/contactUs/page.js
@@ -8,11 +8,6 @@ export default function ContactUs() {
     message: "",
   });
 
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleSelected = () => {
-    setIsOpen(!isOpen);
-  };
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!formData.name || !formData.email || !formData.message) {
@@ -20,91 +15,57 @@ export default function ContactUs() {
       return;
     }
     console.log("Form submitted:", formData);
-    alert("Thank you for contacting us! We will get back to you soon.");
+    alert("Thank you for contacting us!");
     setFormData({ name: "", email: "", message: "" });
   };
+
   return (
-    <main className="bg-gray-100 min-h-screen mt-20 relative flex flex-col">
-      <div className="flex flex-col items-center justify-center font-serif p-6">
-        <h1 className="text-4xl font-bold mt-2 text-orange-500 font-serif">
-          Contact Us
-        </h1>
-        <p className="mt-2 text-lg text-black">
-          Have any questions or concerns? Please fill out the form below.
-        </p>
-      </div>
+    <main className="bg-white min-h-screen pt-40 font-serif flex flex-col items-center">
+      <h1 className="text-7xl font-bold text-black text-center mb-12">
+        Contact Us
+      </h1>
 
       <form
         onSubmit={handleSubmit}
-        className="flex flex-col items-center justify-center gap-4 p-6  font-serif"
+        className="flex flex-col gap-6 w-full max-w-md"
       >
         <input
           type="text"
           placeholder="Your Name"
           value={formData.name}
-          onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-          className="p-2 border border-gray-700 rounded text-black"
+          onChange={(e) =>
+            setFormData({ ...formData, name: e.target.value })
+          }
+          className="p-3 border border-gray-400 rounded text-black"
         />
 
         <input
           type="email"
-          placeholder="Email Address"
+          placeholder="Your Email"
           value={formData.email}
-          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-          className="p-2 border border-gray-700 rounded text-black"
+          onChange={(e) =>
+            setFormData({ ...formData, email: e.target.value })
+          }
+          className="p-3 border border-gray-400 rounded text-black"
         />
 
         <textarea
-          rows="4"
-          cols="50"
-          type="text"
+          rows={5}
           placeholder="Your Message"
           value={formData.message}
           onChange={(e) =>
             setFormData({ ...formData, message: e.target.value })
           }
-          className="p-2 border border-gray-700 rounded text-black"
+          className="p-3 border border-gray-400 rounded text-black resize-none"
         />
 
         <button
           type="submit"
-          className="bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded mt-4"
+          className="bg-orange-500 hover:bg-orange-600 text-white font-bold py-3 px-6 rounded"
         >
           Submit
         </button>
       </form>
-      <div className="flex flex-col items-center justify-center font-serif p-6 text-black font-bold cursor-pointer">
-        <p onClick={toggleSelected}> Frequently Asked Questions </p>
-        <div
-          className={`bg-white p-4 rounded shadow-md mt-4 w-full max-w-2xl ${
-            isOpen ? "" : "hidden"
-          }`}
-        >
-          <ul className="list-disc list-inside">
-            <li> How do I place an order? </li>
-            <li> What payment methods are accepted? </li>
-            <li> What is the delivery process? </li>
-            <li> What is the return policy? </li>
-          </ul>
-        </div>
-      </div>
-      <footer
-        className={`flex absolute flex-col bottom-0 justify-center w-full font-serif  text-orange-500 gap-4 p-6 ${
-          isOpen ? "hidden" : ""
-        }`}
-      >
-        <div>
-          <div className="text-2xl flex flex-row gap-4 items-center justify-center">
-            <p>156 SpringBluff Drive, Calgary, AB, Canada |</p>
-            <p>Email: skLiquor2019@.com |</p>
-            <p>Phone: +1 403-555-0123</p>
-          </div>
-
-          <p className="text-center font-bold">
-            Business Hours: Mon-Fri 9am - 9pm, Sat-Sun 10am - 6pm
-          </p>
-        </div>
-      </footer>
     </main>
   );
 }
