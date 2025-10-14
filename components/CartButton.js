e5b401cf61a9b41a10c704222a4dfddbca9c9759
sharmarishav8879 <Rishav.Sharma01@edu.sait.ca>
2025-10-13 21:34:02 -0600

    Created a new bell feature to notify customer that they have a new reply

--- components/CartButton.js
+++ components/CartButton.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useCart } from "../app/context/CartProvider";
+import { ShoppingCart } from "lucide-react";
 
 export default function CartButton() {
   const { count, setOpen } = useCart();
@@ -11,7 +12,9 @@ export default function CartButton() {
       className="relative inline-flex items-center justify-center"
       title="Cart"
     >
-      <span style={{ fontSize: 22 }}>🛒</span>
+      <span style={{ fontSize: 22 }}>
+        <ShoppingCart />
+      </span>
       {count > 0 && (
         <span
           className="absolute -top-2 -right-2 rounded-full text-white text-xs px-1.5"
